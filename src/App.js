9c9f23b91bca4b53b8374b0ccc95ee3c68659ebc
Tory Dannenberg <rfdberg@gmail.com>
2018-09-10 12:06:30 -0600

    Add intiial html, style login and begin writing Oauth fetch call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { authorizeUser } from './helpers/apiCalls.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+  handleLogin = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Who 
+          you
+          know.</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, please login.
         </p>
+        <button 
+          className="login-button" 
+          onClick={this.handleLogin}>
+          Login
+        </button>
       </div>
     );
   }
---
+++ src/helpers/keys.js
@@ -0,0 +1,3 @@
+export const apiKey = '78qsbaxd3xwguq'; 
+
+export const clientSecret = 'i9G4ZFkc4QsjShmc';
