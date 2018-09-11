4bcac51c480f5ae6e371b560e9e50e1f24d0400a
Tory Dannenberg <rfdberg@gmail.com>
2018-09-10 19:07:42 -0600

    Add login oauth for using auth0 WIP :construction:

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import { authorizeUser } from './helpers/apiCalls.js';
+import Auth from './Auth/Auth.js';
+const auth = new Auth();
+
 
 class App extends Component {
-  constructor() {
-    super();
-
-  }
 
   handleLogin = () => {
-    console.log('click');
+    auth.login();
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Who 
+          <h1 className="App-title">Who
           you
           know.</h1>
         </header>
         <p className="App-intro">
           To get started, please login.
         </p>
-        <button 
-          className="login-button" 
+        <button
+          className="login-button"
           onClick={this.handleLogin}>
           Login
         </button>
