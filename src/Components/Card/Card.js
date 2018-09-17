415828e002aa5311966cf9226f419ec45d8bd48a
Tory Dannenberg <rfdberg@gmail.com>
2018-09-17 10:13:07 -0600

    Add mockdata to conditionally render cards and implement upvote functionality

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,22 +2,32 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-  return (
-    <div className='card-component'>
-      <div className='card-info-section'>
-        <img src="https://pbs.twimg.com/profile_images/512675211309633536/lYHh4p7W_400x400.jpeg" alt="Smiley face" className="circle-image"></img>
-        <h3 className=''>Chet Manley</h3>
-        <h6>ISIS Field Agent</h6>
-        <h6>Georgetown University</h6>
-        <hr className='card-hr'></hr>
+  
+  if (props.cards.length) {
+    return (
+      <div className='card-component'>
+        <div className='card-info-section'>
+          <img src={props.cards[0].imgUrl} alt="Smiley face" className="circle-image"></img>
+          <h3 className=''>{props.cards[0].name}</h3>
+          <h6>{props.cards[0].title}</h6>
+          <h6>{props.cards[0].school}</h6>
+          <hr className='card-hr'></hr>
+        </div>
+        <div className='card-button-container'>
+          <button className='circle-buttons'>No Clue</button>
+          <button className='circle-buttons'>Vague Idea</button>
+          <button className='circle-buttons' onClick={() => { props.handleUpVote() }}>BFF</button>
+        </div>
       </div>
-      <div className='card-button-container'>
-        <button className='circle-buttons'>No Clue</button>
-        <button className='circle-buttons'>Vague Idea</button>
-        <button className='circle-buttons'>BFF</button>
+    );
+  } else {
+    return (
+      <div>
+      No cards to sort
       </div>
-    </div>
-  );
+    );
+  }
+ 
 };
 
 export default Card;
--- src/Components/Sort/Sort.js
+++ src/Components/Sort/Sort.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card.js';
+import mockData from '../../helpers/mockData';
 
 export default class Sort extends Component {
   constructor() {
     super();
 
+    this.state = {
+      cards: []
+    };
   }
+
+  async componentDidMount() {
+    const cards = await this.fetchCardsToSort();
+    this.setState({
+      cards
+    });
+  }
+
+  fetchCardsToSort = () => {
+    console.log('fetch sort');
+    return mockData;
+  }
+
+  getNewCard = () => {
+    const newCards = [...this.state.cards];
+    let cardToSave = newCards.shift();
+    this.setState({
+      cards: newCards
+    });
+    return cardToSave;
+  }
+
+  handleUpVote = () => {
+    console.log('handleUpvote');
+    let cardToSave = this.getNewCard();
+    console.log(cardToSave);
+  }
+
 
   render() {
     return (
       <div>
-        <Card />
+        <Card cards={this.state.cards} handleUpVote={this.handleUpVote}/>
       </div>
     );
   }
