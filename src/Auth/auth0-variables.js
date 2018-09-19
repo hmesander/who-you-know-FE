0a2911ecb2093b47a417c4cb4a50a2ddf91a5efc
Tory Dannenberg <rfdberg@gmail.com>
2018-09-19 12:22:17 -0600

    Change callback url for oauth back to local for development

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'who-you-know-fe.auth0.com',
   clientId: 'pImHOKv37JREokHwv2khQhVivu91YvyJ',
-  callbackUrl: 'https://who-you-know-fe.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 };
