0533f81620fe72e17b3da62390cb46519520121b
Tory Dannenberg <rfdberg@gmail.com>
2018-09-19 11:09:52 -0600

    Update url

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'who-you-know-fe.auth0.com',
   clientId: 'pImHOKv37JREokHwv2khQhVivu91YvyJ',
-  callbackUrl: 'https://who-you-know-be.herokuapp.com/callback'
+  callbackUrl: 'https://who-you-know-fe.herokuapp.com/callback'
 };
