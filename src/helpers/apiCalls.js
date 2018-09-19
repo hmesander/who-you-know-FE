a5d78cadce7b7f040b4007cc3448ba30e11ca503
Tory Dannenberg <rfdberg@gmail.com>
2018-09-19 10:48:36 -0600

    Update url

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -1,6 +1,6 @@
 
 export const postTokens = async (accessToken, idToken) => {
-  const response = await fetch(`https://who-you-know.herokuapp.com/api/v1/users?api_key=${accessToken}&id_token=${idToken}`, {
+  const response = await fetch(`https://who-you-know-be.herokuapp.com/api/v1/users?api_key=${accessToken}&id_token=${idToken}`, {
     method: 'POST'
   });
   const results = await response.json();
@@ -8,7 +8,7 @@ export const postTokens = async (accessToken, idToken) => {
 };
 
 export const getConnectionsForUser = async (userId) => {
-  let url = `https://who-you-know.herokuapp.com/api/v1/users/${userId}/cards`;
+  let url = `https://who-you-know-be.herokuapp.com/api/v1/users/${userId}/cards`;
   const response = await fetch(url, {
     method: 'GET'
   });
@@ -17,7 +17,7 @@ export const getConnectionsForUser = async (userId) => {
 };
 
 export const updateDifficultyLevel = async (userId, cardId, difficultyLevel) => {
-  let url = `https://who-you-know.herokuapp.com/api/v1/users/${userId}/cards/${cardId}?difficulty=${difficultyLevel}`;
+  let url = `https://who-you-know-be.herokuapp.com/api/v1/users/${userId}/cards/${cardId}?difficulty=${difficultyLevel}`;
   const response = await fetch(url, {
     method: 'PUT'
   });
@@ -26,7 +26,7 @@ export const updateDifficultyLevel = async (userId, cardId, difficultyLevel) => 
 };
 
 export const retrieveCardsToPlay = async (userId, difficultyLevel) => {
-  let url = `https://who-you-know.herokuapp.com/api/v1/users/${userId}/cards?difficulty=${difficultyLevel}`;
+  let url = `https://who-you-know-be.herokuapp.com/api/v1/users/${userId}/cards?difficulty=${difficultyLevel}`;
   const response = await fetch(url, {
     method: 'GET'
   });
